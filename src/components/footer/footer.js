4ca578832867f3df78d67c18d4f843d1317c8947
ida <idasusannajohansson@gmail.com>
2023-01-03 10:12:50 +0100

    Added header, footer and login components

---
+++ src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import "../../shared/global/style.css"
+import "./footer.css"
+
+export const Footer = () => {
+    return (
+        <>
+        © Copyright Päron AB 2023
+        </>
+    )
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import "../../shared/global/style.css"
+import "./header.css"
+
+export const Header = () => {
+    return (
+        <>
+        <div className="header-container">
+        PÄRON
+        </div>
+        </>
+    )
+}
