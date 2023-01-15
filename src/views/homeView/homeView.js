59de0043758968161d755cc1eecef83a9c6a3406
ida <idasusannajohansson@gmail.com>
2023-01-15 17:10:16 +0100

    Added WarehouseName and ProductName to stockform.

--- src/views/homeView/homeView.js
+++ src/views/homeView/homeView.js
@@ -3,7 +3,7 @@ import "../../shared/global/style.css"
 import { Header } from "../../components/header/header";
 import { Footer } from "../../components/footer/footer";
 import { DeliveryForm } from "../../components/deliveryForm/deliveryForm";
-import { StockForm, StockQuantity } from "../../components/stockForm/stockForm";
+import { StockQuantity } from "../../components/stockForm/stockForm";
 
 export const HomeView = () => {
  
